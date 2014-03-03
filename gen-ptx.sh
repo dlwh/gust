b51ccfe6bc598a902cba936693ea4ac033c0de0a
David Hall <dlwh@cs.berkeley.edu>
2014-03-02 21:05:26 -0800

    gen-ptx

---
+++ gen-ptx.sh
@@ -0,0 +1,9 @@
+PATH="$PATH:/Developer/NVIDIA/CUDA-5.0/bin/"
+
+if [ `uname` == Darwin ]; then
+  NVCC_OPTS="-ccbin /usr/bin/llvm-g++-4.2 $NVCC_OPTS"
+fi
+
+nvcc $NVCC_OPTS -D TYPE=float -arch sm_21 --ptx src/main/resources/snap/linalg/cuda/map_kernels_float.cu -o src/main/resources/snap/linalg/cuda/map_kernels_float.ptx
+nvcc $NVCC_OPTS -D TYPE=double -arch sm_21 --ptx src/main/resources/snap/linalg/cuda/map_kernels_float.cu -o src/main/resources/snap/linalg/cuda/map_kernels_double.ptx
+
