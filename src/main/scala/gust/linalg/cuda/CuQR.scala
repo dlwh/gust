bfcbbb6f989d34054c5342a564352d86cf1a36d2
piotrMocz <piotr.moczurad@gmail.com>
2014-06-29 01:07:37 +0200

    Full factors for the new QR

--- src/main/scala/gust/linalg/cuda/CuQR.scala
+++ src/main/scala/gust/linalg/cuda/CuQR.scala
@@ -1,6 +1,6 @@
 package gust.linalg.cuda
 
-import jcuda.jcublas.cublasHandle
+import jcuda.jcublas.{JCublas2, cublasHandle}
 import breeze.linalg.{DenseMatrix, DenseVector}
 import org.netlib.util.intW
 import jcuda.driver.{CUfunction, CUmodule, JCudaDriver}
@@ -14,8 +14,111 @@ import com.github.fommil.netlib.LAPACK.{getInstance => lapack}
  */
 object CuQR {
 
+  def QRFactorsFloat(A: CuMatrix[Float], tau: DenseVector[Float])(implicit handle: cublasHandle): (CuMatrix[Float], CuMatrix[Float]) = {
+    if (A.rows < A.cols) {
+      println("Number of rows of matrix A cannot be smaller than the number of columns.")
+      return (A, null)
+    }
+
+    val m = A.rows
+    val n = A.cols
+    val es = A.elemSize.toInt
+
+    val d_R = CuMatrix.create[Float](m, n); d_R := A          // triangular factor
+    val d_A = CuMatrix.create[Float](m, n); d_A := A          // copy of A
+    val d_Q = CuMatrix.fromDense(DenseMatrix.eye[Float](m))   // orthogonal factor
+    val d_H = CuMatrix.create[Float](m, m)                    // placeholder for reflectors
+    val d_diag = CuMatrix.ones[Float](m, 1)                   // we'll use it to set/update the diagonal
+
+    val tauArr = Array(0.0f)
+    val tauPtr = jcuda.Pointer.to(tauArr)
+    val zeroArr = Array(0.0f)
+    val zero = jcuda.Pointer.to(zeroArr)
+    val oneArr = Array(1.0f)
+    val one = jcuda.Pointer.to(oneArr)
+    val minusOneArr = Array(1.0f)
+    val minusOne = jcuda.Pointer.to(minusOneArr)
+
+    // zero out everything below the diagonal in d_R
+    // and everything above (including) diagonal in d_A
+    // TODO: kernels to copy the triangles
+    zeroOutFloat(d_R, 'L')
+    zeroOutFloat(d_A, 'U', true)
+
+    // set the diagonal in d_A to ones:
+    JCublas2.cublasScopy(handle, n, d_diag.offsetPointer, 1, d_A.offsetPointer, d_A.majorStride+1)
+
+    cfor(0)(_ < tau.length, _ + 1) { i => {
+      tauArr(0) = -tau(i)
+
+      // d_H = -tau(i) * d_A(:, i) * d_A(:, i)'
+      SgemmNT(m, m, 1, tauPtr, d_A, 0, i, d_A, 0, i, zero, d_H, 0, 0)
+
+      // d_H = d_H + I
+      JCublas2.cublasSaxpy(handle, m, minusOne, d_diag.offsetPointer, 1, d_H.offsetPointer, d_H.majorStride+1)
+
+      // d_Q *= d_H
+      SgemmNN(m, m, m, one, d_Q, 0, 0, d_H, 0, 0, zero, d_Q, 0, 0)
+    }}
+
+    (d_Q, d_R)
+  }
+
+  def QRFactorsDouble(A: CuMatrix[Double], tau: DenseVector[Double])(implicit handle: cublasHandle): (CuMatrix[Double], CuMatrix[Double]) = {
+    if (A.rows < A.cols) {
+      println("Number of rows of matrix A cannot be smaller than the number of columns.")
+      return (A, null)
+    }
+
+    val m = A.rows
+    val n = A.cols
+    val es = A.elemSize.toInt
+
+    val d_R = CuMatrix.create[Double](m, n); d_R := A               // triangular factor
+    val d_A = CuMatrix.create[Double](m, n); d_A := A               // copy of A
+    val d_Q = CuMatrix.fromDense(DenseMatrix.eye[Double](m))        // orthogonal factor
+    val d_H = CuMatrix.create[Double](m, m)                         // placeholder for reflectors
+    val d_diag = CuMatrix.fromDense(DenseMatrix.ones[Double](m, 1)) // we'll use it to set/update the diagonal
+
+    val tauArr = Array(0.0)
+    val tauPtr = jcuda.Pointer.to(tauArr)
+    val zeroArr = Array(0.0)
+    val zero = jcuda.Pointer.to(zeroArr)
+    val oneArr = Array(1.0)
+    val one = jcuda.Pointer.to(oneArr)
+    val minusOneArr = Array(1.0)
+    val minusOne = jcuda.Pointer.to(minusOneArr)
+
+    // zero out everything below the diagonal in d_R
+    // and everything above (including) diagonal in d_A
+    // TODO: kernels to copy the triangles
+    zeroOutDouble(d_R, 'L')
+    zeroOutDouble(d_A, 'U', true)
+
+    // set the diagonal in d_A to ones:
+    JCublas2.cublasDcopy(handle, n, d_diag.offsetPointer, 1, d_A.offsetPointer, d_A.majorStride+1)
+
+    cfor(0)(_ < tau.length, _ + 1) { i => {
+      tauArr(0) = -tau(i)
+
+      // d_H = -tau(i) * d_A(:, i) * d_A(:, i)'
+      DgemmNT(m, m, 1, tauPtr, d_A, 0, i, d_A, 0, i, zero, d_H, 0, 0)
+
+      // d_H = d_H + I
+      JCublas2.cublasDaxpy(handle, m, minusOne, d_diag.offsetPointer, 1, d_H.offsetPointer, d_H.majorStride+1)
+
+      // d_Q *= d_H
+      DgemmNN(m, m, m, one, d_Q, 0, 0, d_H, 0, 0, zero, d_Q, 0, 0)
+    }}
+
+    (d_Q, d_R)
+  }
+
   /**
-   * QR factorization for matrices of size m x n (where m >= n)
+   * QR factorization for matrices of size m x n (where m >= n) (Float version)
+   * The result is returned (as in LAPACK) in form of a matrix containing both
+   * the triangular factor and the reflectors and a vector tau.
+   * To construct the full Q and R factors, one has to use the 'QRFactors' method.
    * @param A
    * @param handle
    * @return
@@ -137,7 +240,7 @@ object CuQR {
 
 
   /**
-   * QR factorization for matrices of size m x n (where m >= n)
+   * QR factorization for matrices of size m x n (where m >= n) (Double version)
    * @param A
    * @param handle
    * @return
